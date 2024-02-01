3ccc2487e89e7e8801df3810829d541114fe6eb8
Maksym Solomkin <maksym@solomk.in>
2024-01-31 22:57:06 -0500

    Style footer

--- next-personal-site/src/components/footer.tsx
+++ next-personal-site/src/components/footer.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import {cn} from "@/lib/utils";
+import Link from "next/link";
 
 export const Footer = () => {
     return (
-        <footer className={cn('fixed', 'inset-x-0', 'bottom-0', 'flex', 'justify-center', 'items-center', 'h-12', 'bg-gray-800', 'text-white')}>
-            © 2024 Maksym Solomkin
+        <footer className="flex items-center justify-between p-6 border-t">
+            <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Maksym Solomkin</p>
+            <nav className="flex gap-4">
+                <Link className="text-xs hover:underline underline-offset-4" href="https://github.com/solomkinmv">
+                    GitHub
+                </Link>
+                <Link className="text-xs hover:underline underline-offset-4" href="https://twitter.com/solomkinmv">
+                    Twitter
+                </Link>
+            </nav>
         </footer>
     );
 };
