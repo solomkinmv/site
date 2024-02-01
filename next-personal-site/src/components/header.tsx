3613b751d8fb7060bdee3b352100092d7b198c0c
Maksym Solomkin <maksym@solomk.in>
2024-01-31 23:07:53 -0500

    Style header

--- next-personal-site/src/components/header.tsx
+++ next-personal-site/src/components/header.tsx
@@ -2,22 +2,50 @@ import React from 'react';
 import {cn} from "@/lib/utils";
 import Image from "next/image";
 import Link from 'next/link';
+import {Button} from "@/components/ui/button";
 
 export const Header = () => {
     return (
-        <header className={cn('flex', 'justify-between', 'items-center', 'h-12', 'px-4', 'bg-gray-800', 'text-white')}>
-            <div className={cn('flex', 'items-center')}>
-                <Image src="/logo-192.png" width={192} height={192} alt="Icon" className={cn('h-8', 'w-8', 'mr-2')} />
-                <span>Maksym Solomkin</span>
-            </div>
-            <nav>
-                <ul className={cn('flex', 'space-x-4')}>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/posts">Blog</Link></li>
-                    <li><Link href="/tags">Tags</Link></li>
-                    <li><Link href="https://github.com/solomkinmv">GitHub</Link></li>
-                </ul>
+        <header className="flex items-center justify-between p-6 border-b">
+            <Link className="flex items-center gap-2" href="#">
+                {/*<MountainIcon className="h-6 w-6"/>*/}
+                <Image src="/logo-192.png" alt="Icon" className="h-6 w-6" width={192} height={192} />
+                <span className="text-lg font-semibold">Maksym Solomkin</span>
+            </Link>
+            <nav className="flex gap-4">
+                <Link className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/">
+                    Home
+                </Link>
+                <Link className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/posts">
+                    Posts
+                </Link>
+                <Link className="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50" href="/tags">
+                    Tags
+                </Link>
             </nav>
+            <Button className="rounded-full" size="icon" variant="outline">
+                <MoonIcon className="h-6 w-6"/>
+                <span className="sr-only">Toggle theme</span>
+            </Button>
         </header>
     );
 };
+
+function MoonIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+        </svg>
+    )
+}
