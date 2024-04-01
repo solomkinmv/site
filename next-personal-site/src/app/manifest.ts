ad85a3542a14f1c78bd061cc241a80d080642244
Maksym Solomkin <maksym@solomk.in>
2024-03-31 20:30:19 -0400

    Add manifest and sitemap

---
+++ next-personal-site/src/app/manifest.ts
@@ -0,0 +1,20 @@
+import {MetadataRoute} from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Maksym Solomkin',
+        short_name: 'Maksym Solomkin',
+        description: 'Personal website of Maksym Solomkin',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#fff',
+        theme_color: '#fff',
+        icons: [
+            {
+                src: '/favicon.ico',
+                sizes: 'any',
+                type: 'image/x-icon',
+            },
+        ],
+    }
+}
