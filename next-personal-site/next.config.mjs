dc6f94a673c4fedb3cf8a2a1087e0d22fd1ad371
Maksym Solomkin <maksym@solomk.in>
2025-12-20 17:54:36 -0500

    chore: update dependencies

--- next-personal-site/next.config.mjs
+++ next-personal-site/next.config.mjs
@@ -1,9 +1,4 @@
-import remarkGfm from 'remark-gfm'
 import createMDX from '@next/mdx'
-import rehypeSlug from 'rehype-slug'
-import rehypeStringify from "rehype-stringify";
-import rehypePrettyCode from "rehype-pretty-code";
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,23 +8,20 @@ const nextConfig = {
   },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   reactStrictMode: true,
-  experimental: {
-    mdxRs: false,
-  }
 }
 
 const withMDX = createMDX({
   options: {
     remarkPlugins: [
-      remarkGfm,
+      'remark-gfm',
     ],
     rehypePlugins: [
-      rehypeStringify,
-      [rehypePrettyCode, {
+      'rehype-stringify',
+      ['rehype-pretty-code', {
         theme: "catppuccin-frappe",
       }],
-      rehypeSlug,
-      [rehypeAutolinkHeadings, {
+      'rehype-slug',
+      ['rehype-autolink-headings', {
         behavior: 'wrap'
       }],
     ],
